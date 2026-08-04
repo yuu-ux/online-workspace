c611d3e58ca028d987c1b3763c377f424bce77c5
yonuma28 <kisaragi.12056@gmail.com>
2026-08-04 15:26:22 +0900

    fix: デフォルトのformLogin を削除

--- src/main/java/com/example/online_workspace/SwaggerUiSecurityConfig.java
+++ src/main/java/com/example/online_workspace/SwaggerUiSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.online_workspace;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,8 +19,7 @@ public class SwaggerUiSecurityConfig {
 					"/v3/api-docs/swagger-config"
 				).permitAll()
 				.anyRequest().authenticated()
-			)
-			.formLogin(withDefaults());
+			);
 		return http.build();
 	}
 }
