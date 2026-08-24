0da4ee299dbb2371008f1335869639495cd2eda6
yuu-ux <yuugaehara@gmail.com>
2026-08-24 23:45:51 +0900

    fix: 重複したFlyway V2マイグレーションを削除

--- src/main/resources/db/migration/V2__add_room_list_indexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX idx_rooms_public_list
-    ON rooms (status_id, visibility_id, created_at DESC, id DESC);
-
-CREATE INDEX idx_blocks_blocked_blocker
-    ON blocks (blocked_user_id, blocker_user_id);
